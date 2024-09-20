4a7a2af2448d155ab631ee3fd5aa52ea05ab4d43
willSchnere <schnerwill@gmail.com>
2024-09-20 17:13:55 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+
+import { createRoot } from 'react-dom/client'
+import HelloWord from './assets/HelloWord.jsx'
+import './index.css'
+import Home from './Pages/Home.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <Home />
+)
